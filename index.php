365fba14b5cbc15e268f0c39b466351963cfacf9
v7dobra <shherbina.i@gmail.com>
2017-05-10 17:54:30 +0300

    лендинг для продажи противоугонных систем

---
+++ index.php
@@ -0,0 +1,149 @@
+<!doctype html>
+<html lang="">
+<head>
+	<link type="text/css" href="/media/css/style.css" rel="stylesheet">
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+	<!-- bxSlider Javascript file -->
+	<script src="lib/jquery.bxslider.min.js"></script>
+	<!-- bxSlider CSS file -->
+	<link href="lib/jquery.bxslider.css" rel="stylesheet"/>
+
+
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>AutoLocker</title>
+	<style>
+		.top-logo{
+			background-color: #00a0d2;
+			height: 80px;
+			width: 100%;
+
+		}
+		.main-screen{
+			background-color: #8A987E;
+			height: 800px;
+			width: 100%;
+			}
+		.statistics{
+			background-color: #C1780F;
+			height: 800px;
+			width: 100%;
+
+		}
+
+		.description-block{
+			background-color: #C1780F;
+			height: 800px;
+			width: 100%;
+
+		}
+		.advantage-block{
+			background-color: #C1780F;
+			height: 800px;
+			width: 100%;
+
+		}
+
+		.product-block{
+			background-color: #C1780F;
+			height: 800px;
+			width: 100%;
+
+		}
+		.reviews-block{
+			background-color: #C1780F;
+			height: 800px;
+			width: 100%;
+
+		}
+		.affilate-program{
+			background-color: #C1780F;
+			height: 400px;
+			width: 100%;
+
+		}
+
+
+
+
+
+
+	</style>
+
+</head>
+<body>
+
+
+<div class="main-wrapper" id="main-page-wrapper">
+	<div class="site-content-container">
+		<div class="row grid-system">
+			<div class="inner-box grid-system top-logo">
+
+			</div>
+
+		</div>
+
+		<div class="row grid-system">
+			<div class="inner-box grid-system main-screen">
+
+			</div>
+
+		</div>
+		<div class="row grid-system">
+			<div class="inner-box grid-system statistics">
+
+			</div>
+
+		</div>
+		<div class="row grid-system">
+			<div class="inner-box grid-system description-block">
+
+
+			</div>
+		</div>
+		<div class="row grid-system">
+			<div class="inner-box grid-system advantage-block">
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+
+		<div class="row grid-system">
+			<div class="inner-box grid-system product-block">
+
+
+			</div>
+		</div>
+
+		<div class="row grid-system">
+			<div class="inner-box grid-system reviews-block">
+
+
+			</div>
+
+		</div>
+		<div class="row grid-system">
+			<div class="inner-box grid-system affilate-program">
+
+			</div>
+
+		</div>
+
+		<div class="row grid-system">
+			<div class="inner-box grid-system footer-form">
+
+			</div>
+
+		</div>
+
+
+	</div>
+</div>
+
+
+</body>
+</html>
